f7eddf8b32617aed531de5563186c9478c9eac35
Simon Sicard <ssicard@e1z2r7p3.42.us.org>
2017-05-07 19:20:46 -0700

    ressources and command

---
+++ command.sh
@@ -0,0 +1,10 @@
+# -------------- VM  version 1.1 ------------- #
+#                                              #
+# 42 / filler VM Developped by: Hcao - Abanlin #
+#                                              #
+# -------------------------------------------- #
+launched resources/players/grati.filler
+$$$ exec p1 : [resources/players/grati.filler]
+error:
+bad player
+./filler_vm -p1 resources/players/grati.filler -p2 ./filler -f resources/maps/map00
